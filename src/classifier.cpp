4966cc519073485570feafbd4f6fa4611ce2ed70
wa-ty <connorbwaity@gmail.com>
2021-11-15 13:44:43 -0600

    clean

--- src/classifier.cpp
+++ src/classifier.cpp
@@ -29,17 +29,11 @@ bool Classifier::detect_person(cv::Mat in)
   float confidence_threshold = 0.2;
 
   std::vector<int> indices[num_classes];
-  std::vector<cv::Rect> boxes[num_classes];
   std::vector<float> scores[num_classes];
 
   for (auto& output : detections) {
-    const auto num_boxes = output.rows;
-    for (int i = 0; i < num_boxes; i++) {
-      auto x = output.at<float>(i, 0) * in.cols;
-      auto y = output.at<float>(i, 1) * in.rows;
-      auto width = output.at<float>(i, 2) * in.cols;
-      auto height = output.at<float>(i, 3) * in.rows;
-      cv::Rect rect(x - width/2, y - height/2, width, height);
+    const auto detections = output.rows;
+    for (int i = 0; i < detections; i++) {
       for (int c = 0; c < num_classes; c++) {
           auto confidence = *output.ptr<float>(i, 5 + c);
           if ( confidence >= confidence_threshold && class_names[c] == "person" ) {
